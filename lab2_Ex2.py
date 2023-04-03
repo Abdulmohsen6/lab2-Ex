0c7e55657cb8864daa303806341ee5af3a2080ad
Abdulmohsen6 <129119866+Abdulmohsen6@users.noreply.github.com>
2023-04-03 19:58:47 +0300

    Add files via upload

---
+++ lab2_Ex2.py
@@ -0,0 +1,15 @@
+from nltk.tokenize import sent_tokenize
+para="""Cake is a form of sweet food made from 
+flour, sugar, and other ingredients, that is 
+usually baked.In their oldest forms, cakes were 
+modifications of bread, but cakes now cover a wide 
+range of preparations that can be simple or 
+elaborate, and that share features with other 
+desserts such as pastries, meringues, custards, and 
+pies.The most commonly used cake ingredients 
+include flour, sugar, eggs, butter or oil or 
+margarine, a liquid, and leavening agents, such as 
+baking soda or baking powder."""
+tokenized_para=sent_tokenize(para)
+print(tokenized_para)
+print(type(tokenized_para))
